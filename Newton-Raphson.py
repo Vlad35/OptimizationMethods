62ef49a0038f79a6d3d539f6b3a911b9e1c91143
Vlad35 <90512038+Vlad35@users.noreply.github.com>
2023-05-01 21:54:33 +0300

    Create Newton-Raphson.py

---
+++ Newton-Raphson.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+# Определение функции f(x,y)
+def f(x):
+    return x[0]**2 + 8*x[1]**2 - x[0]*x[1] + x[0]
+
+# Определение градиента функции f(x,y)
+def grad_f(x):
+    return np.array([2*x[0]-x[1]+1, 16*x[1]-x[0]])
+
+# Определение гессиана функции f(x,y)
+def hess_f(x):
+    return np.array([[2, -1], [-1, 16]])
+
+# Метод Ньютона-Рафсона
+def newton_raphson(f, grad_f, hess_f, x0, eps1, eps2, max_iter):
+    x = x0
+    iter_count = 0
+    while iter_count < max_iter:
+        grad = grad_f(x)
+        grad_norm = np.linalg.norm(grad)
+        if grad_norm < eps1:
+            break
+        hess = hess_f(x)
+        d = np.linalg.solve(hess, -grad)
+        alpha = 1
+        while f(x + alpha * d) > f(x):
+            alpha /= 2
+        x = x + alpha * d
+        iter_count += 1
+    return x, iter_count, grad_norm
+
+# Тестирование метода Ньютона-Рафсона на функции f(x,y)
+x0 = np.array([1.5, 0.1])
+eps1 = 1e-6
+eps2 = 1e-6
+max_iter = 1000
+
+x_min, iter_count, grad_norm = newton_raphson(f, grad_f, hess_f, x0, eps1, eps2, max_iter)
+
+print("Минимум функции:", x_min)
+print("Число итераций:", iter_count)
+print("Норма градиента:", grad_norm)
